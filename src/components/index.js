36500cbe7358f6776c97dcb608b4bac5879303e0
Charithra Jayasinghe <charithraseneth3@gmail.com>
2023-03-06 23:36:01 +0530

    first

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Billing from './Billing'
+import Business from './Business';
+import Button from './Button';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import FeedBackCard from './FeedBackCard';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Hero from './Hero';
+import NavBar from './NavBar';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+
+export {
+    NavBar,
+    Billing,
+    CardDeal,
+    CTA,
+    Business,
+    Button,
+    Clients,
+    FeedBackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials
+};
+
